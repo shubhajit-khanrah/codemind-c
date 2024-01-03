250a74f345b7572b7d89291395ff3cd88afcc4b9
shubhajit-khanrah <155384856+shubhajit-khanrah@users.noreply.github.com>
2024-01-03 21:45:50 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n];     for(int i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     int a,b,sum=0;     scanf("%d %d",&a,&b);     for(int i=0;i<n;i++)     {         if(arr[i]<a || arr[i]>b)         {             sum=sum+arr[i];         }     }     printf("%d",sum); }

--- Sum_of_elements_not_between_A_and_B.c
+++ Sum_of_elements_not_between_A_and_B.c
@@ -8,44 +8,14 @@ int main()
     {
         scanf("%d",&arr[i]);
     }
-    int a,b;
+    int a,b,sum=0;
     scanf("%d %d",&a,&b);
-    int found=0;
     for(int i=0;i<n;i++)
     {
         if(arr[i]<a || arr[i]>b)
         {
-            printf("%d ",arr[i]);
-            found=1;
+            sum=sum+arr[i];
         }
     }
-    if(!found)
-    {
-        printf("-1");
-    }
-}#include<stdio.h>
-int main()
-{
-    int n;
-    scanf("%d",&n);
-    int arr[n];
-    for(int i=0;i<n;i++)
-    {
-        scanf("%d",&arr[i]);
-    }
-    int a,b;
-    scanf("%d %d",&a,&b);
-    int found=0;
-    for(int i=0;i<n;i++)
-    {
-        if(arr[i]<a || arr[i]>b)
-        {
-            printf("%d ",arr[i]);
-            found=1;
-        }
-    }
-    if(!found)
-    {
-        printf("-1");
-    }
+    printf("%d",sum);
 }
