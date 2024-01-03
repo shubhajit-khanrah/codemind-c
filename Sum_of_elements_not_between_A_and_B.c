3416a4939d89b52db58bd207d1a511b1939fd6c0
shubhajit-khanrah <155384856+shubhajit-khanrah@users.noreply.github.com>
2024-01-03 21:42:45 +0530

    #include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n];     for(int i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     int a,b;     scanf("%d %d",&a,&b);     int found=0;     for(int i=0;i<n;i++)     {         if(arr[i]<a || arr[i]>b)         {             printf("%d ",arr[i]);             found=1;         }     }     if(!found)     {         printf("-1");     } }#include<stdio.h> int main() {     int n;     scanf("%d",&n);     int arr[n];     for(int i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     int a,b;     scanf("%d %d",&a,&b);     int found=0;     for(int i=0;i<n;i++)     {         if(arr[i]<a || arr[i]>b)         {             printf("%d ",arr[i]);             found=1;         }     }     if(!found)     {         printf("-1");     } }

---
+++ Sum_of_elements_not_between_A_and_B.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int a,b;
+    scanf("%d %d",&a,&b);
+    int found=0;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]<a || arr[i]>b)
+        {
+            printf("%d ",arr[i]);
+            found=1;
+        }
+    }
+    if(!found)
+    {
+        printf("-1");
+    }
+}#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int a,b;
+    scanf("%d %d",&a,&b);
+    int found=0;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]<a || arr[i]>b)
+        {
+            printf("%d ",arr[i]);
+            found=1;
+        }
+    }
+    if(!found)
+    {
+        printf("-1");
+    }
+}
